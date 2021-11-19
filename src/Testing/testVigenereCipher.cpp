92fd82ba300bab5a1cf618a3107e276c2b0fae9c
JackDS <jack.sanders@cern.ch>
2021-11-19 11:31:21 +0000

    Added tests

--- src/Testing/testVigenereCipher.cpp
+++ src/Testing/testVigenereCipher.cpp
@@ -3,3 +3,12 @@
 
 #include "VigenereCipher.hpp"
 
+TEST_CASE("Vigenere Cipher encryption", "[vigenere]") {
+  VigenereCipher cc{"hello"};
+  REQUIRE( cc.applyCipher("QWERTYUIOPASDFGHJKLZXCVBNM", CipherMode::Encrypt) == "AACBXWEMMZEQNJERNIVDVMZZNM");
+}
+
+TEST_CASE("Vigenere Cipher decryption", "[vigenere]") {
+  VigenereCipher cc{"hello"};
+  REQUIRE( cc.applyCipher("AACBXWEMMZEQNJERNIVDVMZZNM", CipherMode::Decrypt) == "QWERTYUIOPASDFGHJKLZXCVBNM");
+}
